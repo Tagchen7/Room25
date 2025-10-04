8e81561f7f38d86a60c0ea85a19cd3cd3f75a625
Tagchen7 <140506078+Tagchen7@users.noreply.github.com>
2025-10-04 12:07:11 +0200

    restructering to use pygame

---
+++ Game/GUI/__init__.py
@@ -0,0 +1,2 @@
+# Makes the directory a Python package
+# Imports the package of the selected application
--- Game/__main__.py
+++ Game/__main__.py
@@ -2,20 +2,8 @@
 # It initializes the game logic and GUI components,
 # starts the game loop, and handles any interactions between them.
 
-from Game.GameLogic.game import Game
-
 def main():
-    """
-    if len(sys.argv) > 1 and sys.argv[1] == "html":
-        ## later ...
-        # from Game.GUI.web_gui import web_gui as GUI
-    else:
-        from Game.GUI.tkinter_gui import tkinter_gui as GUI
-    """
-    from Game.GUI.tkinter_gui.tkinter_gui import TkinterGUI as GUI
-
-    game_loop = Game(GUI)
-    game_loop.run()
+    pass
 
 if __name__ == "__main__":
     main()
