6897c0b777b1e4310b536dc6b2e0c48700b89bf4
Tagchen7 <140506078+Tagchen7@users.noreply.github.com>
2025-10-06 14:13:47 +0200

    improved performance

--- Game/GameLogic/entity.py
+++ Game/GameLogic/entity.py
@@ -42,22 +42,39 @@ def get_color_name(color=ROOMCOLOR["grey"]):
         return "Unknown"
 
 class Base_Room(pygame.sprite.Sprite):
-    sprite_size = (50, 50)
-    def __init__(self, corner=False, color=ROOMCOLOR["grey"], number=0):
+    def __init__(self, sprite_size = (50, 50), corner=False, color=ROOMCOLOR["grey"], number=0):
         super().__init__()
         # Center = (0, 0)
+        self.rect = pygame.Rect((0,0), sprite_size)
         self.corner = corner
-        self.color = color
-        self.number = number
+        self._color = color
+        self._number = number
         self.update_image()
-        self.rect = self.image.get_rect()
+    
+    @property
+    def color(self):
+        return self._color
+    
+    @color.setter
+    def color(self, color):
+        self._color = color
+        self.update_image()
+
+    @property
+    def number(self):
+        return self._number
+    
+    @number.setter
+    def number(self, number):
+        self._number = number
+        self.update_image()
 
     def update_image(self):
         if self.name() == "Unknown":
             self.image = pygame.image.load("Game/Assets/Unknown.png")
         else:
             self.image = pygame.image.load(f"Game/Assets/{get_color_name(self.color)}_{self.number}.png")
-        self.image = pygame.transform.scale(self.image, self.sprite_size)
+        self.image = pygame.transform.scale(self.image, self.rect.size)
 
     def __repr__(self) -> str:
         if self.number > 0:
@@ -96,50 +113,77 @@ class Base_Room(pygame.sprite.Sprite):
         return names.get((self.color, self.number), "Unknown")
     	
     def draw(self, surface):
-        self.update_image()
         surface.blit(self.image, self.rect)
 
 class Text_Sprite(pygame.sprite.Sprite):
-    sprite_size = (50, 50)
-    def __init__(self, color, text=None):
+
+    def __init__(self, sprite_size, color, text=None):
         super().__init__()
+        self.image = pygame.image.load(f"Game/Assets/Color.png")
+        self.image = pygame.transform.scale(self.image, sprite_size)
+        self.rect = self.image.get_rect()
         self.text = text
         self.color = color
-        self.image = pygame.image.load(f"Game/Assets/Color.png")
-        self.image = pygame.transform.scale(self.image, self.sprite_size)
-        self.rect = self.image.get_rect()
-        self.update_image()
     
-    def update_image(self):
-        self.image = pygame.transform.scale(self.image, self.rect.size)
+    @property
+    def color(self):
+        return self._color
+    
+    @color.setter
+    def color(self, color):
+        self._color = color
         self.image.fill(self.color)
+    
+    @property
+    def text(self):
+        return self._text
+    
+    @text.setter
+    def text(self, text):
+        self._text = text
+        self.rendered_text = pygame.font.SysFont('arial', 15).render(f"{self.text}", True, (0,0,0))
+
+    
+    def change_image(self, width=None, height=None, size=None, color=None, text=None):
+        # if size changes, update image size
+        # size overwrites height/width params
+        if height != None or width != None or size != None:
+            if height != None:
+                self.rect.height = height
+            if width != None:
+                self.rect.width = width
+            if size != None:
+                self.rect.size = size
+            self.image = pygame.transform.scale(self.image, self.rect.size)
+            self.rect = self.image.get_rect()
+        if color != None:
+            self.color = color
+        if text != None:
+            self.text = text
 
     def draw(self, surface):
-        self.update_image()
         surface.blit(self.image, self.rect)
         if self.text:
-            text = pygame.font.SysFont('arial', 15).render(f"{self.text}", True, (0,0,0))
-            surface.blit(text, (self.rect.x + self.rect.width/2 - text.get_width()/2, self.rect.y + self.rect.height/2 - text.get_height()/2))
+            surface.blit(self.rendered_text, (self.rect.x + self.rect.width/2 - self.rendered_text.get_width()/2, self.rect.y + self.rect.height/2 - self.rendered_text.get_height()/2))
 
 class Player(Text_Sprite):
-    def __init__(self, color, number=0):
-        super().__init__(color=color)
+    def __init__(self, sprite_size, color, number=0, selected_sprite_size=(40, 40)):
+        super().__init__(sprite_size=sprite_size, color=color)
         self.color = color
         self.number = number
         self.is_selected = False
+        self.is_selected_image = pygame.image.load(f"Game/Assets/O.png")
+        self.is_selected_image = pygame.transform.scale(self.is_selected_image, selected_sprite_size)
 
     def draw(self, surface):
         self.text = self.number
         super().draw(surface)
         if self.is_selected:
-            image = pygame.image.load(f"Game/Assets/O.png")
-            image = pygame.transform.scale(image, (40, 40))
-            surface.blit(image, (self.rect.centerx - image.get_width()/2, self.rect.centery - image.get_height()/2))
+            surface.blit(self.is_selected_image, (self.rect.centerx - self.is_selected_image.get_width()/2, self.rect.centery - self.is_selected_image.get_height()/2))
 
 class Info(Text_Sprite):
-    sprite_size = (50, 20)
-    def __init__(self, color, player=None):
-        super().__init__(color=color)
+    def __init__(self, sprite_size, color, player=None):
+        super().__init__(sprite_size=sprite_size, color=color)
         self.player = player
         self.color = color
     
@@ -151,34 +195,42 @@ class Info(Text_Sprite):
     def player(self, player):
         self._player = player
         if player:
-            self.text = player.number
+            if player.number > 0:
+                self.text = player.number
+            else:
+                self.text = None
         else:
             self.text = None
 
 class Room(Base_Room):
-    sprite_size = (50, 50)
     show_info = False
-    def __init__(self, corner=False, color=ROOMCOLOR["grey"], number=0):
-        super().__init__(corner=corner, color=color, number=number)
+    def __init__(self, sprite_size=(50, 50), info_height=20, corner=False, color=ROOMCOLOR["grey"], number=0):
+        super().__init__(sprite_size=sprite_size, corner=corner, color=color, number=number)
         # whenever someone looks at the room, add the info based on what they see
+        self.info_height = info_height
         self.info = []
         self.is_selected = False
         self.was_selected = 0
 
     def add_info(self, color, player):
-        self.info.append(Info(color=color, player=player))
+        self.info.append(Info(sprite_size=(self.info_height, self.info_height), color=color, player=player))
+        self.update_info()
 
     def remove_info(self):
         if self.info:
             self.info.pop()
+            self.update_info()
+
+    def update_info(self):
+        new_sprite_width = self.rect.width / len(self.info)
+        for i, info in enumerate(self.info):
+            info.change_image(height=self.info_height, width=new_sprite_width)
+            info.rect.bottomleft = (self.rect.bottomleft[0] + i * info.rect.width, self.rect.bottomleft[1])
     
     def draw(self, surface):
-        self.update_image()
-        surface.blit(self.image, self.rect)
+        super().draw(surface)
         if self.number == 0 or self.show_info:
-            for i, info in enumerate(self.info):
-                info.rect.width = self.sprite_size[1] / len(self.info)
-                info.rect.bottomleft = (self.rect.bottomleft[0] + i * info.rect.width, self.rect.bottomleft[1])
+            for info in self.info:
                 info.draw(surface)
         if self.corner and self.name() == "Unknown":
             pygame.draw.circle(surface, "blue", self.rect.center, 7.5)
@@ -189,22 +241,29 @@ class Room(Base_Room):
             
 
 class Shift_Arrow(pygame.sprite.Sprite):
-    sprite_size = (50, 50)
-    def __init__(self, direction, number):
+    def __init__(self, direction, number, sprite_size=(50, 50)):
         super().__init__()
         self.direction = direction
         self.number = number
         self.consecutive_clicks = 0
         self.image = pygame.image.load(f"Game/Assets/Shift_Arrow.png")
-        self.image = pygame.transform.scale(self.image, self.sprite_size)
+        self.image = pygame.transform.scale(self.image, sprite_size)
         self.image = pygame.transform.rotate(self.image, 90 * direction)
         self.rect = self.image.get_rect()
+
+    @property
+    def consecutive_clicks(self):
+        return self._consecutive_clicks
+    
+    @consecutive_clicks.setter
+    def consecutive_clicks(self, consecutive_clicks):
+        self._consecutive_clicks = consecutive_clicks
+        self.rendered_text = pygame.font.SysFont('arial', 15).render(f"{self.consecutive_clicks}", True, (0,0,0))
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
         if self.consecutive_clicks > 0:
-            text = pygame.font.SysFont('arial', 15).render(f"{self.consecutive_clicks}", True, (0,0,0))
-            surface.blit(text, (self.rect.x + self.rect.width/2 - text.get_width()/2, self.rect.y + self.rect.height/2 - text.get_height()/2))
+            surface.blit(self.rendered_text, (self.rect.x + self.rect.width/2 - self.rendered_text.get_width()/2, self.rect.y + self.rect.height/2 - self.rendered_text.get_height()/2))
     
     def __eq__(self, other):
         if isinstance(other, Shift_Arrow):
@@ -213,19 +272,18 @@ class Shift_Arrow(pygame.sprite.Sprite):
     
 class Grid:
     size = 5
-    room_size = (50, 50)
-    def __init__(self):
+    def __init__(self, room_size=(50, 50)):
         # self.rooms have a tuple denoting position as key (x, y) = (0, 1) and a room at the position
         # room positions are in range(size)
         # center room is always B1
-        Room.sprite_size = self.room_size
+        self.room_size = room_size
         self.rooms = {}
         for x in range(5):
             cx = x - self.size//2
             for y in range(5):
                 cy = y - self.size//2
                 corner = (cx!=0 and cy!=0) and (abs(cx) != 1 or abs(cy) != 1)
-                self.rooms[(x, y)] = Room(corner=corner)
+                self.rooms[(x, y)] = Room(corner=corner, sprite_size=self.room_size)
                 if cx == 0 and cy == 0:
                     self.rooms[(x, y)].color = ROOMCOLOR["blue"]
                     self.rooms[(x, y)].number = 1
@@ -233,10 +291,10 @@ class Grid:
         for i in range(self.size):
             ci = i - self.size//2
             if ci != 0:
-                self.arrows.append(Shift_Arrow(0, i))  # right
-                self.arrows.append(Shift_Arrow(1, i))  # down
-                self.arrows.append(Shift_Arrow(2, i))  # left
-                self.arrows.append(Shift_Arrow(3, i))  # up
+                self.arrows.append(Shift_Arrow(direction=0, number=i, sprite_size=self.room_size))  # right
+                self.arrows.append(Shift_Arrow(direction=1, number=i, sprite_size=self.room_size))  # down
+                self.arrows.append(Shift_Arrow(direction=2, number=i, sprite_size=self.room_size))  # left
+                self.arrows.append(Shift_Arrow(direction=3, number=i, sprite_size=self.room_size))  # up
 
     def shift_rooms(self, direction:int, num:int):
         # direction, number == Shift_Arrow.direction, Shift_Arrow.number
@@ -289,22 +347,23 @@ class Grid:
         Room.show_info = not Room.show_info
 
 class Room_Notes():
-    sprite_size = (50, 50)
     starting_center = (50, 400)
 
-    def __init__(self):
-        self.blue_rooms = [Base_Room(color=ROOMCOLOR["blue"], number=i) for i in range(1, 4)]
-        self.green_rooms = [Base_Room(color=ROOMCOLOR["green"], number=i) for i in range(1, 8)]
-        self.yellow_rooms = [Base_Room(color=ROOMCOLOR["yellow"], number=i) for i in range(1, 9)]
-        self.red_rooms = [Base_Room(color=ROOMCOLOR["red"], number=i) for i in range(1, 9)]
-        self.undo_room = Base_Room(color=ROOMCOLOR["grey"], number=0)
+    def __init__(self, sprite_size=(50, 50)):
+        self.sprite_size = sprite_size
+        self.blue_rooms = [Base_Room(color=ROOMCOLOR["blue"], number=i, sprite_size=sprite_size) for i in range(1, 4)]
+        self.green_rooms = [Base_Room(color=ROOMCOLOR["green"], number=i, sprite_size=sprite_size) for i in range(1, 8)]
+        self.yellow_rooms = [Base_Room(color=ROOMCOLOR["yellow"], number=i, sprite_size=sprite_size) for i in range(1, 9)]
+        self.red_rooms = [Base_Room(color=ROOMCOLOR["red"], number=i, sprite_size=sprite_size) for i in range(1, 9)]
+        self.undo_room = Base_Room(color=ROOMCOLOR["grey"], number=0, sprite_size=sprite_size)
         for room in self.all_rooms():
             room.rect = room.image.get_rect()
+        self.init_rect_pos()
 
     def all_rooms(self):
         return self.blue_rooms + self.green_rooms + self.yellow_rooms + self.red_rooms + [self.undo_room]
 
-    def draw(self, surface):
+    def init_rect_pos(self):
         for room in self.blue_rooms:
             room.rect.center = (self.starting_center[0] + self.sprite_size[0] * (room.number - 1), self.starting_center[1])
         self.undo_room.rect.center = (self.starting_center[0] + self.sprite_size[0] * 6, self.starting_center[1])
@@ -314,21 +373,25 @@ class Room_Notes():
             room.rect.center = (self.starting_center[0] + self.sprite_size[0] * (room.number - 1), self.starting_center[1] + 2 * self.sprite_size[1])
         for room in self.red_rooms:
             room.rect.center = (self.starting_center[0] + self.sprite_size[0] * (room.number - 1), self.starting_center[1] + 3 * self.sprite_size[1])
+
+    def draw(self, surface):
         for room in self.all_rooms():
             room.draw(surface)
 
 class Color_Notes():
-    sprite_size = (50, 50)
     starting_center = (400, 50)
 
-    def __init__(self):
-        self.notes = [Info(color=color) for color in ROOMCOLOR.values()]
-        self.undo_note = Info(color=ROOMCOLOR["white"], player=None)
-        self.swap_note = Info(color=ROOMCOLOR["white"], player=None)
+    def __init__(self, sprite_size = (50, 50), symbol_sprite_size = (30, 30)):
+        self.sprite_size = sprite_size
+        self.notes = [Info(color=color, sprite_size=sprite_size, player=None) for color in ROOMCOLOR.values()]
+        self.undo_note = Info(color=ROOMCOLOR["white"], sprite_size=sprite_size, player=None)
+        self.swap_note = Info(color=ROOMCOLOR["white"], sprite_size=sprite_size, player=None)
         self.swap = False
-        for note in self.all_notes():
-            note.image = pygame.transform.scale(note.image, self.sprite_size)
-            note.rect = note.image.get_rect()
+        self.undo_image = pygame.image.load(f"Game/Assets/X.png")
+        self.undo_image = pygame.transform.scale(self.undo_image, symbol_sprite_size)
+        self.swap_image = pygame.image.load(f"Game/Assets/Swap.png")
+        self.swap_image = pygame.transform.scale(self.swap_image, symbol_sprite_size)
+        self.init_rect_pos()
     
     @property
     def swap(self):
@@ -345,30 +408,30 @@ class Color_Notes():
     def all_notes(self):
         return self.notes + [self.undo_note] + [self.swap_note]
 
-    def draw(self, surface):
+    def init_rect_pos(self):
         for i, note in enumerate(self.all_notes()):
             note.rect.center = (self.starting_center[0], self.starting_center[1] + self.sprite_size[1] * i)
             if note == self.swap_note:
                 note.rect.center = (self.starting_center[0] - self.sprite_size[0], self.starting_center[1] + self.sprite_size[1] * (i-1))
+
+    def draw(self, surface):
+        for note in self.all_notes():
             note.draw(surface)
             if note == self.undo_note:
-                image = pygame.image.load(f"Game/Assets/X.png")
-                image = pygame.transform.scale(image, (30, 30))
-                surface.blit(image, (note.rect.centerx - image.get_width()/2, note.rect.centery - image.get_height()/2))
+                surface.blit(self.undo_image, (note.rect.centerx - self.undo_image.get_width()/2, note.rect.centery - self.undo_image.get_height()/2))
             if note == self.swap_note:
-                image = pygame.image.load(f"Game/Assets/Swap.png")
-                image = pygame.transform.scale(image, (30, 30))
-                surface.blit(image, (note.rect.centerx - image.get_width()/2, note.rect.centery - image.get_height()/2))
+                surface.blit(self.swap_image, (note.rect.centerx - self.swap_image.get_width()/2, note.rect.centery - self.swap_image.get_height()/2))
 
 class Player_Notes():
-    sprite_size = (50, 50)
     starting_center = (500, 0)
     selection_center = (100, 200)
 
-    def __init__(self) -> None:
+    def __init__(self, sprite_size=(50, 50), selected_sprite_size=(40, 40)) -> None:
+        self.sprite_size = sprite_size
         self.players = []
-        self.possible_players = [Player(color=color, number=0) for color in PLAYERCOLOR.values()]
-        self.players = [Player(color=color, number=i+1) for i, color in enumerate(PLAYERCOLOR.values())]
+        self.possible_players = [Player(color=color, number=0, sprite_size=sprite_size, selected_sprite_size=selected_sprite_size) for color in PLAYERCOLOR.values()]
+        self.players = [Player(color=color, number=i+1, sprite_size=sprite_size, selected_sprite_size=selected_sprite_size) for i, color in enumerate(PLAYERCOLOR.values())]
+        self.update_rect_pos()
 
     def draw_selection(self, surface):
         for i, player in enumerate(self.possible_players):
@@ -383,8 +446,12 @@ class Player_Notes():
             if player.number == 0:
                 self.players.remove(player)
         self.players = sorted(self.players, key=lambda player: player.number)
+        self.update_rect_pos()
+
+    def update_rect_pos(self):
+        for player in self.players:
+            player.rect.center = (self.starting_center[0], self.starting_center[1] + self.sprite_size[1] * player.number)
 
     def draw(self, surface):
         for player in self.players:
-            player.rect.center = (self.starting_center[0], self.starting_center[1] + self.sprite_size[1] * player.number)
             player.draw(surface)
